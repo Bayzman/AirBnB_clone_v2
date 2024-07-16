537600c0357c3c382fb3be73323d02ce0fd1488c
Bayzman <bassmaninno102@gmail.com>
2024-07-15 17:06:35 -0700

    prepare web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,9 +5,7 @@ sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
+# Using the -p flag creates parent directories if they do not already exist
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
@@ -20,6 +18,7 @@ sudo echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
+# Creates a symbolic
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
